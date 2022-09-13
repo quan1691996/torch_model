b89f653303b20024c77b2a9c77a24ebddf14c0a2
ldquan1996 <luongdoanhquan1996@gmail.com>
2022-09-13 18:39:40 +0900

    fix

--- torch_model/YOLO/darknet.py
+++ torch_model/YOLO/darknet.py
@@ -410,7 +410,7 @@ def write(x, batches, results):
     t_size = cv2.getTextSize(label, cv2.FONT_HERSHEY_PLAIN, 1 , 1)[0]
     c2 = c1[0] + t_size[0] + 3, c1[1] + t_size[1] + 4
     cv2.rectangle(img, (int(c1[0]), int(c1[1])), (int(c2[0]), int(c2[1])) ,color, -1)
-    # cv2.putText(img, label, (c1[0], c1[1] + t_size[1] + 4), cv2.FONT_HERSHEY_PLAIN, 1, [225,255,255], 1)
+    cv2.putText(img, label, (int(c1[0]), int(c1[1]) + t_size[1] + 4), cv2.FONT_HERSHEY_PLAIN, 1, [225,255,255], 1)
     return img
 
 list(map(lambda x: write(x, im_batches, orig_ims), output))
